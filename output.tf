e5ddbed98f1a672455a026dfd24801707bc59676
Calvin Hyundong Kim <calvin75@gmail.com>
2020-02-23 20:35:10 +0900

    Update output.tf
    added diags_sa_primary_access_key output variable

--- output.tf
+++ output.tf
@@ -10,6 +10,7 @@ output "diagnostics_map" {
         map(
             "diags_sa", azurerm_storage_account.log.id,
             "diags_sa_blob", azurerm_storage_account.log.primary_blob_endpoint,
+            "diags_sa_primary_access_key", azurerm_storage_account.log.primary_access_key,
             "eh_name",  var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].name : null,
             "eh_id", var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].id : null,
         )
