b87b7b371eb3df97c4e5245774502a064bf7bc9b
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-06-18 12:48:51 +0800

    v13 providers

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-# provider "azurecaf" {
-  
-# }
 
 locals {
   module_tag          = {
     "module" = basename(abspath(path.module))
   }
   tags                = merge(var.tags,local.module_tag)
+}
+
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
 }
